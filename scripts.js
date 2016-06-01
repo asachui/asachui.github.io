5448e5cfd059936f89ddd86a5f932cddea4bb38f
Arnold Chui <asachui1@gmail.com>
2016-06-01 18:03:30 +0900

    Upload file

--- scripts.js
+++ scripts.js
@@ -1,4 +1,27 @@
-var myElement = document.getElementById('myElement');
+// file upload from PC
+function el(id){return document.getElementById(id);} // Get elem by ID
+
+var canvas  = el("canvas");
+var context = canvas.getContext("2d");
+
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           var img = new Image();
+           img.onload = function() {
+             context.drawImage(img, 0, 0);
+           };
+           img.src = e.target.result;
+        };
+        FR.readAsDataURL( this.files[0] );
+    }
+}
+
+el("fileUpload").addEventListener("change", readImage, false);
+
+/*
+var myElement = document.getElementById('mainSection');
 
 // create a simple instance
 // by default, it only adds horizontal recognizers
@@ -8,4 +31,13 @@ var mc = new Hammer(myElement);
 mc.on("panleft panright tap press", function(ev) {
     myElement.textContent = ev.type +" gesture detected.";
 });
+*/
+
+var mc = new Hammer(canvas)
+mc.on("panleft panright tap press", function(ev) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "30px Arial";
+    context.fillText(ev.type + " gesture detected",10,50);
+});
+
 
